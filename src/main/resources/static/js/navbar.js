a1263eca36d357500e5f52cc27a5f9334f826912
Leonardo Perdomo <leonardop763@gmail.com>
2022-11-15 16:49:55 -0500

    Changes applied to all the templates and th js file
    The js file is modified to achieve the different types of navbar links according to the authenticate state.

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,44 +1,141 @@
 const navbarLinks = document.getElementsByClassName('app__navbar-links');
-const navbarDesktopLinks = document.getElementsByClassName('app__navbar-desktop-links');
+const navbarDesktopLinks = document.getElementById('app__navbar-desktop-links');
 
 const menuButton = document.getElementById('app__navbar-menu-btn');
 const mobileMenu = document.getElementById('app__navbar-menu');
 
 
 if (isAuthenticated) {
-    navbarLinks[0].innerHTML = links.map(link =>
-        `<li class="app__flex p-text">
-            <a href="/${link.split(' ').join('')}" class="app__navbar-link">${link}</a>
-        </li>`
-    ).join('');
+    navbarDesktopLinks.innerHTML = `
+        <li class="app__flex p-text">
+            <a href="/secure" th:href="@{/secure}" class="app__navbar-link">
+                Home
+            </a>
+        </li>
+        <li class="app__flex p-text">
+            <a href="/secure/check" th:href="@{/secure/check}" class="app__navbar-link">
+                Check
+            </a>
+        </li>
+    `;
 } else {
-    navbarLinks[0].innerHTML = '';
+    navbarDesktopLinks.innerHTML = '';
 }
 
 
 // Arrow function to show/hide menu
 const toggleMenu = (toggle) => {
     console.log(toggle);
-    if (toggle) {
-        navbarLinks[0].innerHTML = links.map(link =>`
-            <li class="app__flex p-text">
-                <a href="/${link.split(' ').join('')}" class="app__navbar-link">${link}</a>
-            </li>`
-        ).join('');
-        mobileMenu.innerHTML = `
+    if (isAuthenticated) {
+        if (toggle) {
+            mobileMenu.innerHTML = `
             <div id="app__navbar-menu-btn" class="app__nabvar-mobile">
                 <i class="material-symbols-outlined" onclick="toggleMenu(false)">
                     close
                 </i>
                 
                 <ul class="app__navbar-links">
-                    ${navbarLinks[0].innerHTML}
+                    <li class="app__navbar-item">
+                        <a href="/secure" th:href="@{/secure}" class="app__navbar-link">
+                            <i class="material-symbols-outlined">
+                                home
+                            </i>
+                            Home
+                        </a>
+                    </li>
+                    <li class="app__navbar-item">
+                        <a href="/secure/check" th:href="@{/secure/check}" class="app__navbar-link">
+                            <i class="material-symbols-outlined">
+                                check
+                            </i>
+                            Check
+                        </a>
+                    </li>
+                    <li class="app__navbar-item">
+                        <a href="/logout" th:href="@{/logout}" class="app__navbar-link">
+                            <i class="material-symbols-outlined">
+                                logout
+                            </i>
+                            Log Out
+                        </a>
+                    </li>
                 </ul>
-
             </div>
         `;
-    } else if (!toggle) {
-        mobileMenu.innerHTML = ``;
+        } else if (!toggle) {
+            mobileMenu.innerHTML = ``;
+        }
+    } else {
+        if (toggle) {
+            if (pageType === 'login') {
+                mobileMenu.innerHTML = `
+                    <div id="app__navbar-menu-btn" class="app__nabvar-mobile">
+                        <i class="material-symbols-outlined" onclick="toggleMenu(false)">
+                            close
+                        </i>
+                        
+                        <ul class="app__navbar-links">
+                            <li class="app__navbar-item">
+                                <a href="/signup" th:href="@{/sigup}" class="app__navbar-link">
+                                    <i class="material-symbols-outlined">
+                                        person_add
+                                    </i>
+                                    Register
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                `;
+            } else if (pageType === 'signup') {
+                mobileMenu.innerHTML = `
+                    <div id="app__navbar-menu-btn" class="app__nabvar-mobile">
+                        <i class="material-symbols-outlined" onclick="toggleMenu(false)">
+                            close
+                        </i>
+
+                        <ul class="app__navbar-links">
+                            <li class="app__navbar-item">
+                                <a href="/login" th:href="@{/login}" class="app__navbar-link">
+                                    <i class="material-symbols-outlined">
+                                        login
+                                    </i>
+                                    Login
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                `;
+            } else {
+                mobileMenu.innerHTML = `
+                    <div id="app__navbar-menu-btn" class="app__nabvar-mobile">
+                        <i class="material-symbols-outlined" onclick="toggleMenu(false)">
+                            close
+                        </i>
+                        
+                        <ul class="app__navbar-links">
+                            <li class="app__navbar-item">
+                                <a href="/login" th:href="@{/login}" class="app__navbar-link">
+                                    <i class="material-symbols-outlined">
+                                        login
+                                    </i>
+                                    Log In
+                                </a>
+                            </li>
+                            <li class="app__navbar-item">
+                                <a href="/signup" th:href="@{/sigup}" class="app__navbar-link">
+                                    <i class="material-symbols-outlined">
+                                        person_add
+                                    </i>
+                                    Register
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+            `;
+            }
+        } else if (!toggle) {
+            mobileMenu.innerHTML = ``;
+        }
     }
 }
 
